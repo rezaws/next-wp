8cee113d0ef30ac79616ffec5ea69a7c7d8c1d5b
rezaws <reza.sepidar@gmail.com>
2025-09-03 21:49:47 +0330

    First commit

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -4,8 +4,9 @@ export default function Settings() {
   return (
     <div className="flex h-screen bg-gray-100">
         {/* Sidebar */}
-        <h2 className="text-xl font-bold mb-6"></h2>  1  
+        <h2 className="text-xl font-bold mb-6"></h2>  
         <Sidebar />
+        Settings
     </div>
   )
 }
